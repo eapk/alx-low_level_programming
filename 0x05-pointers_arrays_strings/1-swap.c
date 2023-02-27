0a83a6cd3bdcc12bcdb1b8325ae1cecb4eaa495a
eapk <ekwasi1@outlook.com>
2023-02-27 21:54:22 +0000

    c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,7 +6,7 @@
 void swap_int(int *a, int *b)
 {
 	int temp;
-	temp = *a;/*a is put in place of temp value*/
-	*a = *b;/*a value is now been replace with the value of b */
+	temp = *a; /*a is put in place of temp storage*/
+	*a = *b; /*a value is now been replace with the value of b */
 	*b = temp;/*a value is now b and but in temp*/
 }
