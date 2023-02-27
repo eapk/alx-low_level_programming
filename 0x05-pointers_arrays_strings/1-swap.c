b2be91e7833b973a43535b51c7f0380981de3294
eapk <ekwasi1@outlook.com>
2023-02-27 22:00:53 +0000

    m c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * swap_int - function that swap the value
- * of two integrrs
+ * of two integrrs a and b
  **/
 void swap_int(int *a, int *b)
 {
 int temp;
 temp = *a; /*a is put in place of temp storage*/
-*a = *b; /*a value is now been replace with the value of b */
+*a = *b; /*b value is now put for a */
 *b = temp;/*a value is now b and but in temp*/
 }
