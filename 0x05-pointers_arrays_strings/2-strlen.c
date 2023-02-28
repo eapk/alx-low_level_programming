eb76e8e8f86eb050a3a9d85f030ae3f735da7e0c
eapk <ekwasi1@outlook.com>
2023-02-28 05:27:48 +0000

    mc

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
-/*
- * _strlin(char s) - lenght of a string
- *using strlen
- */
-int _strlen(char *str)
-{
-	/*initializing the string*/
-	
-	int len;
+#include "main.h"
 
-	str = "My First Strlen";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return(0);
+/**
+ * _strlen - A functions that returns the length of a string.
+ * @s: The string to get its length.
+ *
+ * Return: The length of @s
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')/*the array count != the last count of strings*/
+	{
+		length++;/*count length from zero*/
+	}
+
+	return (length);
 }
