42e27b2214a09911c6ec76780e3aa0391b23aa10
eapk <ekwasi1@outlook.com>
2023-02-28 04:58:17 +0000

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,14 @@
  * _strlin(char s) - lenght of a string
  *using strlen
  */
-int _strlen(char *s);
+int _strlen(char *str)
 {
 	/*initializing the string*/
-	char *str;
+	
 	int len;
 
 	str = "My First Strlen";
 	len = _strlen(str);
-	printf('%d\n", len);
+	printf("%d\n", len);
 	return(0);
 }
